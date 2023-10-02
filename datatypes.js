7be0e9f592e47b1054572ffdef72e9cd8f310b53
Nirmal Khadka <chattribeast@gmail.com>
2023-10-03 02:18:38 +0545

    first commit

---
+++ datatypes.js
@@ -0,0 +1,34 @@
+//nn bb ss u
+let a=null;
+let b=true;
+let c=true;
+let d=BigInt('89');
+let e='kadf';
+let f=Symbol('i am nice');
+let g=undefined;
+console.log(a, b, c, d, e , f, g );
+console.log(typeof e);
+
+//object in js
+const item={
+    'harry':true,
+    'su':false,
+    're':67 
+}
+console.log(item['hary']) 
+
+//q1
+let as='nirmal';
+let nb=9;
+console.log(as+nb);
+
+//q2
+document.write(typeof(as+nb))
+
+//q3
+const qw={
+    name:'aksd'
+}
+//name='nirmal';
+qw.room=120;//we can add new in const
+console.log(qw.name);
---
+++ diff_in_2and3_equal.js
@@ -0,0 +1,5 @@
+document.write(1 == '1');
+// it will give true because it only check answer not data types it will only check 1 =1 it will convert them in same data types
+
+document.write(1 === '1');
+//=== will check datatyes also
